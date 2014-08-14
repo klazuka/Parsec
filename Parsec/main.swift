14dc925c04f012f92d738833626bd0979a57a01b
Keith Lazuka <klazuka@acompli.com>
2014-08-13 20:51:32 -0700

    blah

--- Parsec/main.swift
+++ Parsec/main.swift
@@ -1,12 +1,36 @@
-//
-//  main.swift
-//  Parsec
-//
-//  Created by Keith Lazuka on 8/13/14.
-//  Copyright (c) 2014 Acompli. All rights reserved.
-//
-
 import Foundation
 
-println("Hello, World!")
+
+func matchChar(c: Character, s: String) -> (Bool, String) {
+    let idx = s.startIndex
+    if !s.isEmpty && s[idx] == c {
+        return (true, s.substringFromIndex(advance(idx, 1)))
+    } else {
+        return (false, s)
+    }
+}
+
+func matchFoo(input: String) -> (Bool, String) {
+    var ok = false
+    var s = input
+    (ok, s) = matchChar("f", s)
+    if !ok { return (false, s) }
+    (ok, s) = matchChar("o", s)
+    if !ok { return (false, s) }
+    (ok, s) = matchChar("o", s)
+    if !ok { return (false, s) }
+    return (ok, s)
+}
+
+var s = "cfoo"
+var ok = false
+
+(ok, s) = matchChar("c", s)
+println("\(ok), remaining '\(s)'")
+
+(ok, s) = matchFoo(s)
+println("\(ok), remaining '\(s)'")
+
+(ok, s) = matchChar("!", s)
+println("\(ok), remaining '\(s)'")
 
