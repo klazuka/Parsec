13dc858ae8621de600ac549032e4cd0ef961b9e0
Keith Lazuka <klazuka@acompli.com>
2014-08-14 05:49:23 -0700

    Add a 'choice' operator

--- Parsec/main.swift
+++ Parsec/main.swift
@@ -1,9 +1,23 @@
 import Foundation
 
+extension String {
+    var tail: String {
+    get {
+        if self.isEmpty { return self }
+        return self.substringFromIndex(advance(self.startIndex, 1))
+    }
+    }
+}
 
-// doesn't compile
+
+// doesn't compile in Xcode6-beta5
 //typealias Parser<T> = String -> (T, String)
 
+//MARK:- combinators
+
+// sequence operator over Parser actions
+// it's a shame that I can't define a generic typealias to simplify the type signature
+// TODO: represent each Parser action as a callable struct?
 infix operator >>> { associativity left }
 func >>><A,B>(lhs: String -> (A?, String), rhs: String -> (B?, String)) -> String -> (B?, String) {
     return { s in
@@ -19,26 +33,77 @@ func >>><A,B>(lhs: String -> (A?, String), rhs: String -> (B?, String)) -> Strin
     }
 }
 
+
+infix operator <|> { associativity left }
+func <|><A>(lhs: String -> (A?, String), rhs: String -> (A?, String)) -> String -> (A?, String) {
+    return { s in
+        let (a, s2) = lhs(s)
+        if a != nil {
+            return (a, s2) // lhs-success
+        }
+        let (b, s3) = rhs(s)
+        if b != nil {
+            return (b, s3) // rhs-success
+        }
+        return (nil, s) // both sides failed
+    }
+}
+
+
 func run<T>(parser: String -> (T, String), input: String) -> (T, String) {
     return parser(input)
 }
 
+//MARK:- core parsers
 func matchChar(c: Character) -> String -> (Bool?, String) {
     return { s in
-        let idx = s.startIndex
-        if !s.isEmpty && s[idx] == c {
-            return (true, s.substringFromIndex(advance(idx, 1)))
+        if !s.isEmpty && s[s.startIndex] == c {
+            return (true, s.tail)
         } else {
-            return (false, s)
+            return (nil, s)
         }
     }
 }
-    
-func matchFoo() -> String -> (Bool?, String) {
-    return (matchChar("f") >>> matchChar("o")) >>> matchChar("o")
+
+func matchString(m: String) -> String -> (Bool?, String) {
+    return { s in
+        if m.isEmpty {
+            return (true, s)
+        } else if s.isEmpty {
+            return (nil, s)
+        } else if m[m.startIndex] == s[s.startIndex] {
+            return matchString(m.tail)(s.tail)
+        } else {
+            return (nil, s)
+        }
+    }
 }
 
-var s = "cafoo!"
-let parser = matchChar("c") >>> matchChar("a") >>> matchFoo()
-var (ok, s2) = run(parser, s)
-println("\(ok), remaining '\(s2)'")
+//MARK:- example of an "application" layer parser
+func matchFoo() -> String -> (Bool?, String) {
+    return matchChar("f") >>> matchChar("o") >>> matchChar("o")
+}
+
+//MARK:- demo code
+var s = "kungfoo!"
+
+func test<T>(parser: String -> (T?, String), input: String) -> (Bool, String) {
+    var (match, output) = run(parser, input)
+    if match != nil {
+        println("parse success: remaining='\(output)'")
+        return (true, output)
+    } else {
+        println("parse failed: remaining='\(output)'")
+        return (false, output)
+    }
+}
+
+var parser = matchString("kung") >>> matchFoo()
+test(parser, "kungfoo!")
+
+let parser2 = matchChar(".") <|> matchChar("?") <|> matchChar("!")
+test(parser2, "!")
+
+let parser3 = matchChar("z") >>> (matchChar(".") <|> matchChar("?") <|> matchChar("!"))
+test(parser3, "zd")
+
