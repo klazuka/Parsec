ce6cda89f474b1b5d23dd781e6b515789bf08b2a
Keith Lazuka <klazuka@acompli.com>
2014-08-13 21:22:54 -0700

    very basic parsers are now working

--- Parsec/main.swift
+++ Parsec/main.swift
@@ -1,36 +1,44 @@
 import Foundation
 
 
-func matchChar(c: Character, s: String) -> (Bool, String) {
-    let idx = s.startIndex
-    if !s.isEmpty && s[idx] == c {
-        return (true, s.substringFromIndex(advance(idx, 1)))
-    } else {
-        return (false, s)
+// doesn't compile
+//typealias Parser<T> = String -> (T, String)
+
+infix operator >>> { associativity left }
+func >>><A,B>(lhs: String -> (A?, String), rhs: String -> (B?, String)) -> String -> (B?, String) {
+    return { s in
+        let (a, s2) = lhs(s)
+        if a == nil {
+            return (nil, s)
+        }
+        let (b, s3) = rhs(s2)
+        if b == nil {
+            return (nil, s)
+        }
+        return (b, s3)
     }
 }
 
-func matchFoo(input: String) -> (Bool, String) {
-    var ok = false
-    var s = input
-    (ok, s) = matchChar("f", s)
-    if !ok { return (false, s) }
-    (ok, s) = matchChar("o", s)
-    if !ok { return (false, s) }
-    (ok, s) = matchChar("o", s)
-    if !ok { return (false, s) }
-    return (ok, s)
+func run<T>(parser: String -> (T, String), input: String) -> (T, String) {
+    return parser(input)
 }
 
-var s = "cfoo"
-var ok = false
+func matchChar(c: Character) -> String -> (Bool?, String) {
+    return { s in
+        let idx = s.startIndex
+        if !s.isEmpty && s[idx] == c {
+            return (true, s.substringFromIndex(advance(idx, 1)))
+        } else {
+            return (false, s)
+        }
+    }
+}
+    
+func matchFoo() -> String -> (Bool?, String) {
+    return (matchChar("f") >>> matchChar("o")) >>> matchChar("o")
+}
 
-(ok, s) = matchChar("c", s)
-println("\(ok), remaining '\(s)'")
-
-(ok, s) = matchFoo(s)
-println("\(ok), remaining '\(s)'")
-
-(ok, s) = matchChar("!", s)
-println("\(ok), remaining '\(s)'")
-
+var s = "cafoo!"
+let parser = matchChar("c") >>> matchChar("a") >>> matchFoo()
+var (ok, s2) = run(parser, s)
+println("\(ok), remaining '\(s2)'")
